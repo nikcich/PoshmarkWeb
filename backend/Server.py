f6de0a73617c310b6928477627276b3831b1bbfb
Nikolas <nrc170001@utdallas.edu>
2022-02-05 19:13:11 -0600

    PyReact

---
+++ backend/Server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, static_folder="../reactapp/build/static", template_folder="../reactapp/build")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+@cross_origin()
+def hello():
+    return render_template('index.html')
+
+
+@app.route("/data")
+@cross_origin()
+def data():
+    return {"Labels": ["L1", "l2", "l3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
